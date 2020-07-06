d82bc88a66eca4fdb7c622130b72647d17e7a30e
yashmunjal <yashmu45@gmail.com>
2020-07-07 03:24:38 +0530

    traverse

--- src/traverse.js
+++ src/traverse.js
@@ -1,6 +1,25 @@
 const traverseNode =({node,parent,visitor})=>{
 
-    if(visitor[node.type])
+    const methods= visitor[node.type];
+
+
+    if(methods && methods.enter){
+        methods.enter({node,parent});
+    }
+
+    if(node.arguments){
+        traverseArray({array:node.arguments,parent:node,visitor})
+    }
+
+    if(methods && methods.exit){
+        methods.exit({node,parent});
+    }
+
+}
+const traverseArray=({array,parent,visitor})=>{
+    array.forEach(e => {
+        traverseNode({node,parent,visitor});
+    });
 }
 
 const traverse = (node, visitor) => {
@@ -15,9 +34,10 @@ const traverse = (node, visitor) => {
 
 
 module.exports = { traverse };
+/*
 const visitor ={
 VariableDeclaration({node,parent}){
     enter(){}
     exit(){}
 }
-}
+}*/
