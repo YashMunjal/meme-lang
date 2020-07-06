43ddb0f029de8b504efccefc63eba6d885eba102
yashmunjal <yashmu45@gmail.com>
2020-07-07 03:12:51 +0530

    traverse

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,23 @@
-const traverse = (node, visitor) => {};
+const traverseNode =({node,parent,visitor})=>{
+
+    if(visitor[node.type])
+}
+
+const traverse = (node, visitor) => {
+
+
+
+
+};
+
+
+ 
+
 
 module.exports = { traverse };
+const visitor ={
+VariableDeclaration({node,parent}){
+    enter(){}
+    exit(){}
+}
+}
