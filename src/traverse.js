b94d6fa3e8bb999bf90c6c49f4d1838e9fa281a3
yashmunjal <yashmu45@gmail.com>
2020-07-08 23:08:42 +0530

    traverse

--- src/traverse.js
+++ src/traverse.js
@@ -1,43 +1,34 @@
-const traverseNode =({node,parent,visitor})=>{
-
-    const methods= visitor[node.type];
-
-
-    if(methods && methods.enter){
-        methods.enter({node,parent});
+const traverseNode = ({ node, parent, visitor }) => {
+    const methods = visitor[node.type];
+  
+    if (methods && methods.enter) {
+      methods.enter({ node, parent });
     }
-
-    if(node.arguments){
-        traverseArray({array:node.arguments,parent:node,visitor})
+  
+    if (node.arguments) {
+      traverseArray({ array: node.arguments, parent: node, visitor });
     }
-
-    if(methods && methods.exit){
-        methods.exit({node,parent});
+  
+    if (methods && methods.exit) {
+      methods.exit({ node, parent });
     }
-
-}
-const traverseArray=({array,parent,visitor})=>{
-    array.forEach(e => {
-        traverseNode({node,parent,visitor});
+  };
+  
+  const traverseArray = ({ array, parent, visitor }) => {
+    array.forEach(node => {
+      traverseNode({ node, parent, visitor });
     });
-}
-
-const traverse = (node, visitor) => {
-
-
-
-
-};
-
-
- 
-
-
-module.exports = { traverse };
-/*
-const visitor ={
-VariableDeclaration({node,parent}){
-    enter(){}
-    exit(){}
-}
-}*/
+  };
+  
+  const traverse = (node, visitor) => {
+    traverseNode({ node, visitor });
+  };
+  
+  module.exports = { traverse };
+  
+  // const visitor = {
+  //   VariableDeclaration: {
+  //     enter({ node, parent }) {},
+  //     exit() {}
+  //   }
+  // }
