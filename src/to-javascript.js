20e612fe39a3c8d3a0e7b4bf90696f9b74bc13e6
yashmunjal <yashmu45@gmail.com>
2020-07-10 01:44:19 +0530

    to-js

--- src/to-javascript.js
+++ src/to-javascript.js
@@ -1,9 +1,17 @@
 const generate = require('@babel/generator').default;
 const { traverse } = require('./traverse');
 
-const babelVisitor = {};
+const babelVisitor = {
+  CallExpression:{
+    enter({node}){
+      node.callee={type:'Identifier',name:node.name}
+    }
+  }
+};
 
-const toJavaScript = ast => {};
+const toJavaScript = ast => {
+  traverse(ast,babelVisitor);
+};
 
 module.exports = {
   toJavaScript,
